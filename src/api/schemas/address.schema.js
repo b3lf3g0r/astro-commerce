ac8b09dfc03008fa551522fc0a8b3d71bcf179e9
Moobi Kabelo <giftmoobi@gmail.com>
2023-03-09 09:25:20 +0200

    added schemas

---
+++ src/api/schemas/address.schema.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview -
+ * @author: b3lf3g0r
+ * @version: 1.0.0
+ * @since: 2023-03-09
+ * */
+import mongoose from 'mongoose';
+
+const Address = new mongoose.Schema(
+  {
+    street: {
+      type: String,
+      required: true,
+    },
+    suburb: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    province: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    zip: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Customer',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+-
+export default mongoose.model('Address', Address);
---
+++ src/api/schemas/user.schema.js
@@ -0,0 +1,35 @@
+/**
+ * @fileoverview -
+ * @author: b3lf3g0r
+ * @version: 1.0.0
+ * @since: 2023-03-09
+ * */
+import mongoose from 'mongoose';
+
+const User = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    mobile: {
+      type: String,
+      required: false,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'customer', 'merchant'],
+      default: 'customer',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', User);
