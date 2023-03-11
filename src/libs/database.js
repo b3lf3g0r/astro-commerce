f82f615f9a5be9ac7f1c628d62f3bc33c8e8f73b
Moobi Kabelo <giftmoobi@gmail.com>
2023-03-11 17:19:32 +0200

    fixed typo

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,11 +1,11 @@
 /**
  * @fileoverview -
- * @author: afridek
+ * @author: b3lf3g0r
  * @version: 1.0.0
- * @since: 2023-02-28
+ * @since: 2023-03-10
  * */
 import mongoose from 'mongoose';
-import { Redis } from 'ioredis';
+// import { Redis } from 'ioredis';
 import { dbConfig } from '../configs/db.config.js';
 
 export const database = {
@@ -18,11 +18,11 @@ export const database = {
   mongoClose() {
     return mongoose.connection.close();
   },
-  redisClient() {
-    return new Redis({
-      port: dbConfig.redis.port,
-      host: dbConfig.redis.host,
-      db: dbConfig.redis.db,
-    });
-  },
+  // redisClient() {
+  //   return new Redis({
+  //     port: dbConfig.redis.port,
+  //     host: dbConfig.redis.host,
+  //     db: dbConfig.redis.db,
+  //   });
+  // },
 };
