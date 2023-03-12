ac9d3e96880eb63c5ea9b7e62f60afb595d89aae
Moobi Kabelo <giftmoobi@gmail.com>
2023-03-12 23:58:15 +0200

    added customer profile services

--- src/api/services/index.js
+++ src/api/services/index.js
@@ -5,7 +5,7 @@
  * @since 2023-03-10
  * */
 import CustomerAuthService from './customer.auth.service.js';
-// import CustomerService from './customer.service.js';
+import CustomerService from './customer.service.js';
 import PasswordService from './password.service.js';
 
-export { CustomerAuthService, PasswordService };
+export { CustomerService, CustomerAuthService, PasswordService };
